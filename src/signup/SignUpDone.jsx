c95b3a6e2b7edde9409b1e127877c14bf93f176a
rlaisqls <rlaisqls@gmail.com>
2024-03-13 08:23:12 +0900

    핸드폰 비율 대응

--- src/signup/SignUpDone.jsx
+++ src/signup/SignUpDone.jsx
@@ -7,10 +7,10 @@ export const SignUpDone = () => {
           <img className="my-0 mx-auto h-16" alt="logo"
                src="https://github.com/rlaisqls/TIL/assets/81006587/4492b702-5cb6-40af-821d-e264cd82549b"/>
 
-          <img className="h-[300px] my-4"
+          <img className="w-[70%] my-4"
                src="https://github.com/rlaisqls/rlaisqls/assets/81006587/416ba6e7-193d-43a2-89ec-284a86e72e86"
                alt="Thankyou"/>
-          <p className="text-[20px] text-center leading-loose">
+          <p className="text-[1em] text-center leading-loose">
               감사합니다! Xquare 회원가입이 완료되었습니다 :)<br/>
               곧 공개될 학교 프로젝트들을 기대해주세요!<br/>
               심심한 1학년이라면 아래 링크를 한 번 눌러보세용
@@ -18,7 +18,7 @@ export const SignUpDone = () => {
 
           <a href="https://scratched-aries-15e.notion.site/DSM-1-d314aa7ba06642cd999c6b01c7a35cc8?pvs=4">
               <button
-                  className="w-[230px] px-4 py-2 mt-6 text-white bg-[#9650FA] rounded-md hover:bg-[#8640FA] focus:outline-none focus:shadow-outline"
+                  className="w-[100%] px-4 py-2 mt-6 text-white bg-[#9650FA] rounded-md hover:bg-[#8640FA] focus:outline-none focus:shadow-outline"
                   type="button"
               >
                   선배들이 준비한 신입생 로드맵
